1fce80944bbd3ae6c897c3f47e66cb7bf8af9f89
Gui0151 <56264234+Gui0151@users.noreply.github.com>
2020-06-21 03:47:57 -0300

    Add files via upload

--- transformações sonic.js
+++ transformações sonic.js
@@ -1,1 +1,3 @@
-123
+function achou() {document.getElementById("E").style.visibility="visible";}
+function olá_amigo() {document.getElementById("S").style.visibility="visible";}  
+function cadê_a_teté() {document.getElementById("H").style.visibility="visible";}
